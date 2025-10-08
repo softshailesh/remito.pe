14041b4b5eb10f7e73de6295c78e5da69bf88df6
softshailesh <shailesh.p@singhsoft.com>
2025-10-08 11:06:25 +0530

    updates

--- src/pages/front/landingpage.jsx
+++ src/pages/front/landingpage.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Header from './components/header'
 
 const Landingpage = () => {
   return (
     <div>
+        <Header />
       AAAAAAAAAAAAAAAAAAAAAAA
     </div>
   )
